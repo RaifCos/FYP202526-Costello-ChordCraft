7caa269dcf0e709ca1ebc5eb2a6422af11887fd5
RaifCos <RaifCostelloBusiness@gmail.com>
2026-03-09 14:40:13 +0000

    Define functions in DisplayManager

--- Application/app/src/main/java/com/example/chordcraft/components/DisplayManager.kt
+++ Application/app/src/main/java/com/example/chordcraft/components/DisplayManager.kt
@@ -1,1 +1,17 @@
 package com.example.chordcraft.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun createFretBoards(chordList: List<Chord>) {
+    val guitarChords = generateGuitarChords(chordList)
+
+    // TODO: Assemble Fret Boards into a Single UI Element (Horizontally Scrolling Menu?)
+    for(guitarChord in guitarChords) {
+        createFretBoard(guitarChord)
+    }
+}
+
+fun drawFretBoard(guitarChord: GuitarChord?) {
+    // TODO: Draw Fret Board based on Guitar Chord Data
+}
