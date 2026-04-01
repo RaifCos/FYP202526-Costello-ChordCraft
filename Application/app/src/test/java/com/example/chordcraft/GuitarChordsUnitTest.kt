378666458a01506ae7651789fa7a8270bd5f385d
RaifCos <RaifCostelloBusiness@gmail.com>
2026-04-01 19:11:43 +0100

    Guitar Chords Unit Testing

---
+++ Application/app/src/test/java/com/example/chordcraft/GuitarChordsUnitTest.kt
@@ -0,0 +1,45 @@
+package com.example.chordcraft
+
+import com.example.chordcraft.components.Chord
+import com.example.chordcraft.components.GuitarNote
+import com.example.chordcraft.components.generateGuitarChords
+import com.example.chordcraft.components.getCandidatePositions
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GuitarChordsUnitTest {
+
+    @Test
+    fun getCandidatePositions_AssertValidMIDI() {
+        // All Combinations of C4
+        val list = listOf<GuitarNote> (
+            GuitarNote(1, 1),
+            GuitarNote(2, 5),
+            GuitarNote(3, 10),
+            GuitarNote(4, 15),
+            GuitarNote(5, 20),
+        )
+        assertEquals(list, getCandidatePositions(60))
+    }
+
+    @Test
+    fun getCandidatePositions_AssertInvalidMIDI_Negative() {
+        assertTrue(getCandidatePositions(-1).isEmpty())
+    }
+
+    @Test
+    fun getCandidatePositions_AssertInvalidMIDI_TooSmall() {
+        assertTrue(getCandidatePositions(1).isEmpty())
+    }
+
+    @Test
+    fun getCandidatePositions_AssertInvalidMIDI_TooLarge() {
+        assertTrue(getCandidatePositions(670).isEmpty())
+    }
+
+    @Test
+    fun generateGuitarChords_AssertEmptyChords() {
+        assertTrue(generateGuitarChords(emptyList()).isEmpty())
+    }
+}
